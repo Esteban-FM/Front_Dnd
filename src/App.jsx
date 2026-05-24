ffa6863a7eda41b2184d6558a3efd264940fad44
Esteban FM <esteban.fsma@gmail.com>
2026-05-23 23:25:56 -0600

    project creation

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="min-h-screen bg-zinc-900 flex items-center justify-center">
+      <h1 className="text-yellow-500 font-bold text-2xl">⚔️ DnD Session</h1>
+    </div>
+  )
+}
